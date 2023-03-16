e27e6467cf2f0eca3b8fd1d14cc6da5904e7f9d9
CDHSpaceDust <chu0020738@gmail.com>
2023-03-16 11:47:52 +0900

    week_02+

--- week_02/roman_to_cardinal.cpp
+++ week_02/roman_to_cardinal.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <conio.h>
+#include <cstdlib>
 
 using namespace std;
 
@@ -8,19 +8,21 @@ int convertToInt(const string& str) {
     int result = 0;
     try {
         result = stoi(str);
-    } catch (const invalid_argument&) {
+    }
+    catch (const invalid_argument&) {
         throw "Error: 올바른 숫자가 아닙니다.";
-    } catch (const out_of_range&) {
+    }
+    catch (const out_of_range&) {
         throw "Error: 숫자가 너무 큽니다.";
     }
     return result;
 }
 
-string arabicToRoman(int num){
+string arabicToRoman(int num) {
     string roman = "";
     int arabic[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
     string romanNumeral[] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-    
+
     for (int i = 0; i < 13; i++) {
         while (num >= arabic[i]) {
             roman += romanNumeral[i];
@@ -36,7 +38,7 @@ int main() {
     while (true) {
         cout << "숫자를 입력하세요 (종료: esc): ";
         getline(cin, strNum);
-        if (_getch() == 27) { // 단축키 esc 입력
+        if (getchar() == 27) { // 단축키 esc 입력
             break;
         }
         try {
@@ -45,7 +47,8 @@ int main() {
                 throw "Error: 범위 값을 벗어났습니다 (1~3999)";
             }
             cout << "변환된 값 : " << arabicToRoman(num) << endl;
-        } catch (const char* msg) {
+        }
+        catch (const char* msg) {
             cerr << msg << endl;
         }
     }
